f4080dad8a66ed3371bfdda04869b81a08d36377
CanftIn <wwc7033@gmail.com>
2023-12-07 13:18:13 +0800

    update iterators

--- include/Pud/IR/Util/Iterators.h
+++ include/Pud/IR/Util/Iterators.h
@@ -7,7 +7,8 @@
 
 namespace Pud::IR::Util {
 
-/// Iterator wrapper that applies a function to the iterator.
+// 迭代器适配器用于将函数应用到底层迭代器的结果上，
+// 从而实现对迭代元素的自定义处理。
 template <typename It, typename DereferenceFunc, typename MemberFunc>
 struct function_iterator_adaptor {
   It internal;
@@ -15,27 +16,22 @@ struct function_iterator_adaptor {
   MemberFunc m;
 
   using iterator_category = std::input_iterator_tag;
-  using value_type =
-      typename std::remove_reference<decltype(d(*internal))>::type;
+  using value_type = std::remove_reference_t<decltype(d(*internal))>;
   using reference = void;
   using pointer = void;
   using difference_type = typename std::iterator_traits<It>::difference_type;
 
-  /// Constructs an adaptor.
-  /// @param internal the internal iterator
-  /// @param d the dereference function
-  /// @param m the member access function
   function_iterator_adaptor(It internal, DereferenceFunc&& d, MemberFunc&& m)
       : internal(std::move(internal)), d(std::move(d)), m(std::move(m)) {}
 
-  decltype(auto) operator*() { return d(*internal); }
-  decltype(auto) operator->() { return m(*internal); }
+  auto operator*() -> decltype(auto) { return d(*internal); }
+  auto operator->() -> decltype(auto) { return m(*internal); }
 
-  function_iterator_adaptor& operator++() {
+  auto operator++() -> function_iterator_adaptor& {
     internal++;
     return *this;
   }
-  function_iterator_adaptor operator++(int) {
+  auto operator++(int) -> function_iterator_adaptor {
     function_iterator_adaptor<It, DereferenceFunc, MemberFunc> copy(*this);
     internal++;
     return copy;
@@ -43,24 +39,22 @@ struct function_iterator_adaptor {
 
   template <typename OtherIt, typename OtherDereferenceFunc,
             typename OtherMemberFunc>
-  bool operator==(
-      const function_iterator_adaptor<OtherIt, OtherDereferenceFunc,
-                                      OtherMemberFunc>& other) const {
+  auto operator==(const function_iterator_adaptor<OtherIt, OtherDereferenceFunc,
+                                                  OtherMemberFunc>& other) const
+      -> bool {
     return other.internal == internal;
   }
 
   template <typename OtherIt, typename OtherDereferenceFunc,
             typename OtherMemberFunc>
-  bool operator!=(
-      const function_iterator_adaptor<OtherIt, OtherDereferenceFunc,
-                                      OtherMemberFunc>& other) const {
+  auto operator!=(const function_iterator_adaptor<OtherIt, OtherDereferenceFunc,
+                                                  OtherMemberFunc>& other) const
+      -> bool {
     return other.internal != internal;
   }
 };
 
-/// Creates an adaptor that dereferences values.
-/// @param it the internal iterator
-/// @return the adaptor
+// 创建一个解引用适配器，将底层迭代器的值解引用。
 template <typename It>
 auto dereference_adaptor(It it) {
   auto f = [](const auto& v) -> auto& { return *v; };
@@ -69,9 +63,7 @@ auto dereference_adaptor(It it) {
       it, std::move(f), std::move(m));
 }
 
-/// Creates an adaptor that gets the address of its values.
-/// @param it the internal iterator
-/// @return the adaptor
+// 创建一个适配器，返回底层迭代器元素的原始指针。
 template <typename It>
 auto raw_ptr_adaptor(It it) {
   auto f = [](auto& v) -> auto* { return v.get(); };
@@ -80,9 +72,7 @@ auto raw_ptr_adaptor(It it) {
       it, std::move(f), std::move(m));
 }
 
-/// Creates an adaptor that gets the const address of its values.
-/// @param it the internal iterator
-/// @return the adaptor
+// 类似于 raw_ptr_adaptor，但返回常量指针。
 template <typename It>
 auto const_raw_ptr_adaptor(It it) {
   auto f = [](auto& v) -> const auto* { return v.get(); };
@@ -91,9 +81,7 @@ auto const_raw_ptr_adaptor(It it) {
       it, std::move(f), std::move(m));
 }
 
-/// Creates an adaptor that gets the keys of its values.
-/// @param it the internal iterator
-/// @return the adaptor
+// 用于 map 类型的迭代器，返回迭代器元素的键（key）。
 template <typename It>
 auto map_key_adaptor(It it) {
   auto f = [](auto& v) -> auto& { return v.first; };
@@ -102,9 +90,7 @@ auto map_key_adaptor(It it) {
       it, std::move(f), std::move(m));
 }
 
-/// Creates an adaptor that gets the const keys of its values.
-/// @param it the internal iterator
-/// @return the adaptor
+// 类似于 map_key_adaptor，但返回常量引用。
 template <typename It>
 auto const_map_key_adaptor(It it) {
   auto f = [](auto& v) -> const auto& { return v.first; };
