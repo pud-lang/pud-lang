9161890d2a0bbd50228a0f74b466a66b23316c96
CanftIn <wwc7033@gmail.com>
2023-12-14 16:52:04 +0800

    add static impl

--- include/Pud/TypeCheck/TypeCheck.h
+++ include/Pud/TypeCheck/TypeCheck.h
@@ -14,103 +14,106 @@
 #include "Pud/Format/Format.h"
 #include "Pud/TypeCheck/Context.h"
 
-namespace Pud::AST {
+namespace Pud::Type {
 
 // 用于推断表达式类型并执行类型引导转换的访问者类。
-class TypecheckVisitor : public CallbackASTVisitor<ExprPtr, StmtPtr> {
+class TypecheckVisitor
+    : public AST::CallbackASTVisitor<AST::ExprPtr, AST::StmtPtr> {
   // 共享简化上下文的指针，用于在类型检查过程中跟踪标识符。
   std::shared_ptr<TypeContext> ctx;
   // 在当前语句之前插入的语句列表。
-  std::shared_ptr<std::vector<StmtPtr>> prependStmts;
+  std::shared_ptr<std::vector<AST::StmtPtr>> prepend_stmts;
 
   // 用于存储每个新表达式和语句，稍后由 transform 方法返回。
-  ExprPtr resultExpr;
-  StmtPtr resultStmt;
+  AST::ExprPtr result_expr;
+  AST::StmtPtr result_stmt;
 
  public:
   // 应用类型检查到给定的语句。
-  static auto apply(Cache* cache, const StmtPtr& stmts) -> StmtPtr;
+  static auto apply(AST::Cache* cache, const AST::StmtPtr& stmts)
+      -> AST::StmtPtr;
 
  public:
   // 接受类型上下文和要插入的语句列表。
   explicit TypecheckVisitor(
       std::shared_ptr<TypeContext> ctx,
-      const std::shared_ptr<std::vector<StmtPtr>>& stmts = nullptr);
+      const std::shared_ptr<std::vector<AST::StmtPtr>>& stmts = nullptr);
 
  public:
   // 转换表达式和语句。
-  auto transform(ExprPtr& e) -> ExprPtr override;
-  auto transform(const ExprPtr& expr) -> ExprPtr override {
+  auto transform(AST::ExprPtr& e) -> AST::ExprPtr override;
+  auto transform(const AST::ExprPtr& expr) -> AST::ExprPtr override {
     auto e = expr;
     return transform(e);
   }
-  auto transform(StmtPtr& s) -> StmtPtr override;
-  auto transform(const StmtPtr& stmt) -> StmtPtr override {
+  auto transform(AST::StmtPtr& s) -> AST::StmtPtr override;
+  auto transform(const AST::StmtPtr& stmt) -> AST::StmtPtr override {
     auto s = stmt;
     return transform(s);
   }
   // 转换类型表达式。
-  auto transform_type(ExprPtr& expr) -> ExprPtr;
-  auto transform_type(const ExprPtr& expr) -> ExprPtr {
+  auto transform_type(AST::ExprPtr& expr) -> AST::ExprPtr;
+  auto transform_type(const AST::ExprPtr& expr) -> AST::ExprPtr {
     auto e = expr;
     return transform_type(e);
   }
 
  private:
   // 定义对未特别处理的表达式和语句的默认行为。
-  void default_visit(Expr* e) override;
-  void default_visit(Stmt* s) override;
+  void default_visit(AST::Expr* e) override;
+  void default_visit(AST::Stmt* s) override;
 
  private:
-  void visit(NoneExpr*) override;
-  void visit(BoolExpr*) override;
-  void visit(IntExpr*) override;
-  void visit(FloatExpr*) override;
-  void visit(StringExpr*) override;
+  void visit(AST::NoneExpr*) override;
+  void visit(AST::BoolExpr*) override;
+  void visit(AST::IntExpr*) override;
+  void visit(AST::FloatExpr*) override;
+  void visit(AST::StringExpr*) override;
 
   /* Identifier access expressions (access.cpp) */
-  void visit(IdExpr*) override;
-  void visit(DotExpr*) override;
-  auto transform_dot(DotExpr*, std::vector<CallExpr::Arg>* = nullptr)
-      -> ExprPtr;
-  auto get_class_member(DotExpr*, std::vector<CallExpr::Arg>*) -> ExprPtr;
+  void visit(AST::IdExpr*) override;
+  void visit(AST::DotExpr*) override;
+  auto transform_dot(AST::DotExpr*, std::vector<AST::CallExpr::Arg>* = nullptr)
+      -> AST::ExprPtr;
+  auto get_class_member(AST::DotExpr*, std::vector<AST::CallExpr::Arg>*)
+      -> AST::ExprPtr;
   auto find_special_member(const std::string&) -> Pud::Type::TypePtr;
-  auto get_best_overload(Expr*, std::vector<CallExpr::Arg>*)
+  auto get_best_overload(AST::Expr*, std::vector<AST::CallExpr::Arg>*)
       -> Pud::Type::FuncTypePtr;
   auto get_dispatch(const std::string&) -> Pud::Type::FuncTypePtr;
 
   /* Collection and comprehension expressions (collections.cpp) */
-  void visit(TupleExpr*) override;
-  void visit(ListExpr*) override;
-  void visit(SetExpr*) override;
-  void visit(DictExpr*) override;
-  void visit(GeneratorExpr*) override;
+  void visit(AST::TupleExpr*) override;
+  void visit(AST::ListExpr*) override;
+  void visit(AST::SetExpr*) override;
+  void visit(AST::DictExpr*) override;
+  void visit(AST::GeneratorExpr*) override;
   auto transform_comprehension(const std::string&, const std::string&,
-                               std::vector<ExprPtr>&) -> ExprPtr;
+                               std::vector<AST::ExprPtr>&) -> AST::ExprPtr;
 
   /* Conditional expression and statements (cond.cpp) */
-  void visit(IfExpr*) override;
-  void visit(IfStmt*) override;
+  void visit(AST::IfExpr*) override;
+  void visit(AST::IfStmt*) override;
 
   /* Operators (op.cpp) */
-  void visit(UnaryExpr*) override;
-  auto evaluate_static_unary(UnaryExpr*) -> ExprPtr;
-  void visit(BinaryExpr*) override;
-  auto evaluate_static_binary(BinaryExpr*) -> ExprPtr;
-  auto transform_binary_simple(BinaryExpr*) -> ExprPtr;
-  auto transform_binary_is(BinaryExpr*) -> ExprPtr;
+  void visit(AST::UnaryExpr*) override;
+  auto evaluate_static_unary(AST::UnaryExpr*) -> AST::ExprPtr;
+  void visit(AST::BinaryExpr*) override;
+  auto evaluate_static_binary(AST::BinaryExpr*) -> AST::ExprPtr;
+  auto transform_binary_simple(AST::BinaryExpr*) -> AST::ExprPtr;
+  auto transform_binary_is(AST::BinaryExpr*) -> AST::ExprPtr;
   auto get_magic(const std::string&) -> std::pair<std::string, std::string>;
-  auto transform_binary_inplace_magic(BinaryExpr*, bool) -> ExprPtr;
-  auto transform_binary_magic(BinaryExpr*) -> ExprPtr;
-  void visit(PipeExpr*) override;
-  void visit(IndexExpr*) override;
+  auto transform_binary_inplace_magic(AST::BinaryExpr*, bool) -> AST::ExprPtr;
+  auto transform_binary_magic(AST::BinaryExpr*) -> AST::ExprPtr;
+  void visit(AST::PipeExpr*) override;
+  void visit(AST::IndexExpr*) override;
   auto transform_static_tuple_index(const Pud::Type::ClassTypePtr&,
-                                    const ExprPtr&, const ExprPtr&)
-      -> std::pair<bool, ExprPtr>;
+                                    const AST::ExprPtr&, const AST::ExprPtr&)
+      -> std::pair<bool, AST::ExprPtr>;
   auto translate_index(int64_t, int64_t, bool = false) -> int64_t;
   auto slice_adjust_indices(int64_t, int64_t*, int64_t*, int64_t) -> int64_t;
-  void visit(InstantiateExpr*) override;
-  void visit(SliceExpr*) override;
+  void visit(AST::InstantiateExpr*) override;
+  void visit(AST::SliceExpr*) override;
 
   /* Calls (call.cpp) */
   /// Holds partial call information for a CallExpr.
@@ -118,37 +121,38 @@ class TypecheckVisitor : public CallbackASTVisitor<ExprPtr, StmtPtr> {
     bool isPartial = false;        // true if the call is partial
     std::string var;               // set if calling a partial type itself
     std::vector<char> known = {};  // mask of known arguments
-    ExprPtr args = nullptr,
-            kwArgs = nullptr;  // partial *args/**kwargs expressions
+    AST::ExprPtr args = nullptr,
+                 kwArgs = nullptr;  // partial *args/**kwargs expressions
   };
-  void visit(StarExpr*) override;
-  void visit(KeywordStarExpr*) override;
-  void visit(EllipsisExpr*) override;
-  void visit(CallExpr*) override;
-  auto transform_call_args(std::vector<CallExpr::Arg>&) -> bool;
-  auto get_callee_fn(CallExpr*, PartialCallData&)
-      -> std::pair<Pud::Type::FuncTypePtr, ExprPtr>;
-  auto call_reorder_arguments(Pud::Type::FuncTypePtr, CallExpr*,
-                              PartialCallData&) -> ExprPtr;
+  void visit(AST::StarExpr*) override;
+  void visit(AST::KeywordStarExpr*) override;
+  void visit(AST::EllipsisExpr*) override;
+  void visit(AST::CallExpr*) override;
+  auto transform_call_args(std::vector<AST::CallExpr::Arg>&) -> bool;
+  auto get_callee_fn(AST::CallExpr*, PartialCallData&)
+      -> std::pair<Pud::Type::FuncTypePtr, AST::ExprPtr>;
+  auto call_reorder_arguments(Pud::Type::FuncTypePtr, AST::CallExpr*,
+                              PartialCallData&) -> AST::ExprPtr;
   auto typecheck_call_args(const Pud::Type::FuncTypePtr&,
-                           std::vector<CallExpr::Arg>&) -> bool;
-  auto transform_special_call(CallExpr*) -> std::pair<bool, ExprPtr>;
-  auto transform_super_f(CallExpr* expr) -> ExprPtr;
-  auto transform_super() -> ExprPtr;
-  auto transform_ptr(CallExpr* expr) -> ExprPtr;
-  auto transform_array(CallExpr* expr) -> ExprPtr;
-  auto transform_is_instance(CallExpr* expr) -> ExprPtr;
-  auto transform_static_len(CallExpr* expr) -> ExprPtr;
-  auto transform_has_attr(CallExpr* expr) -> ExprPtr;
-  auto transform_get_attr(CallExpr* expr) -> ExprPtr;
-  auto transform_set_attr(CallExpr* expr) -> ExprPtr;
-  auto transform_compile_error(CallExpr* expr) -> ExprPtr;
-  auto transform_tuple_fn(CallExpr* expr) -> ExprPtr;
-  auto transform_type_fn(CallExpr* expr) -> ExprPtr;
-  auto transform_realized_fn(CallExpr* expr) -> ExprPtr;
-  auto transform_static_print_fn(CallExpr* expr) -> ExprPtr;
-  auto transform_has_rtti_fn(CallExpr* expr) -> ExprPtr;
-  auto transform_internal_static_fn(CallExpr* expr) -> std::pair<bool, ExprPtr>;
+                           std::vector<AST::CallExpr::Arg>&) -> bool;
+  auto transform_special_call(AST::CallExpr*) -> std::pair<bool, AST::ExprPtr>;
+  auto transform_super_f(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_super() -> AST::ExprPtr;
+  auto transform_ptr(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_array(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_is_instance(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_static_len(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_has_attr(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_get_attr(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_set_attr(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_compile_error(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_tuple_fn(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_type_fn(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_realized_fn(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_static_print_fn(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_has_rtti_fn(AST::CallExpr* expr) -> AST::ExprPtr;
+  auto transform_internal_static_fn(AST::CallExpr* expr)
+      -> std::pair<bool, AST::ExprPtr>;
   auto get_super_types(const Pud::Type::ClassTypePtr& cls)
       -> std::vector<Pud::Type::ClassTypePtr>;
   void add_function_generics(const Pud::Type::FuncType* t);
@@ -156,47 +160,48 @@ class TypecheckVisitor : public CallbackASTVisitor<ExprPtr, StmtPtr> {
                              Pud::Type::FuncType* fn) -> std::string;
 
   /* Assignments (assign.cpp) */
-  void visit(AssignStmt*) override;
-  void transformUpdate(AssignStmt*);
-  void visit(AssignMemberStmt*) override;
-  auto transform_inplace_update(AssignStmt*) -> std::pair<bool, ExprPtr>;
+  void visit(AST::AssignStmt*) override;
+  void transformUpdate(AST::AssignStmt*);
+  void visit(AST::AssignMemberStmt*) override;
+  auto transform_inplace_update(AST::AssignStmt*)
+      -> std::pair<bool, AST::ExprPtr>;
 
   /* Loops (loops.cpp) */
-  void visit(BreakStmt*) override;
-  void visit(ContinueStmt*) override;
-  void visit(WhileStmt*) override;
-  void visit(ForStmt*) override;
-  auto transform_heterogenous_tuple_for(ForStmt*) -> StmtPtr;
-  auto transform_static_for_loop(ForStmt*) -> StmtPtr;
+  void visit(AST::BreakStmt*) override;
+  void visit(AST::ContinueStmt*) override;
+  void visit(AST::WhileStmt*) override;
+  void visit(AST::ForStmt*) override;
+  auto transform_heterogenous_tuple_for(AST::ForStmt*) -> AST::StmtPtr;
+  auto transform_static_for_loop(AST::ForStmt*) -> AST::StmtPtr;
 
   /* Errors and exceptions (error.cpp) */
-  void visit(TryStmt*) override;
-  void visit(ThrowStmt*) override;
+  void visit(AST::TryStmt*) override;
+  void visit(AST::ThrowStmt*) override;
 
   /* Functions (function.cpp) */
-  void visit(YieldExpr*) override;
-  void visit(ReturnStmt*) override;
-  void visit(YieldStmt*) override;
-  void visit(FunctionStmt*) override;
-  auto partialize_function(const Pud::Type::FuncTypePtr&) -> ExprPtr;
+  void visit(AST::YieldExpr*) override;
+  void visit(AST::ReturnStmt*) override;
+  void visit(AST::YieldStmt*) override;
+  void visit(AST::FunctionStmt*) override;
+  auto partialize_function(const Pud::Type::FuncTypePtr&) -> AST::ExprPtr;
   auto get_func_type_base(size_t) -> std::shared_ptr<Pud::Type::RecordType>;
 
  public:
-  auto make_function_type(FunctionStmt*) -> Pud::Type::FuncTypePtr;
+  auto make_function_type(AST::FunctionStmt*) -> Pud::Type::FuncTypePtr;
 
  private:
   /* Classes (class.cpp) */
-  void visit(ClassStmt*) override;
-  void parse_base_classes(ClassStmt*);
+  void visit(AST::ClassStmt*) override;
+  void parse_base_classes(AST::ClassStmt*);
   auto generate_tuple(size_t, const std::string& = TYPE_TUPLE,
                       std::vector<std::string> = {}, bool = true)
       -> std::string;
 
   /* The rest (typecheck.cpp) */
-  void visit(SuiteStmt*) override;
-  void visit(ExprStmt*) override;
-  void visit(StmtExpr*) override;
-  void visit(CommentStmt* stmt) override;
+  void visit(AST::SuiteStmt*) override;
+  void visit(AST::ExprStmt*) override;
+  void visit(AST::StmtExpr*) override;
+  void visit(AST::CommentStmt* stmt) override;
 
  private:
   /* Type inference (infer.cpp) */
@@ -207,17 +212,17 @@ class TypecheckVisitor : public CallbackASTVisitor<ExprPtr, StmtPtr> {
     auto x = a;
     return unify(x, b);
   }
-  auto infer_types(StmtPtr, bool is_toplevel = false) -> StmtPtr;
+  auto infer_types(AST::StmtPtr, bool is_toplevel = false) -> AST::StmtPtr;
   auto realize(Pud::Type::TypePtr) -> Pud::Type::TypePtr;
   auto realize_func(Pud::Type::FuncType*, bool = false) -> Pud::Type::TypePtr;
   auto realize_type(Pud::Type::ClassType*) -> Pud::Type::TypePtr;
   auto generate_special_ast(Pud::Type::FuncType*)
-      -> std::shared_ptr<FunctionStmt>;
+      -> std::shared_ptr<AST::FunctionStmt>;
   auto get_realization_id(Pud::Type::ClassType*, Pud::Type::FuncType*)
       -> size_t;
   auto make_ir_type(Pud::Type::ClassType*) -> Pud::IR::Types::Type*;
   auto make_ir_function(
-      const std::shared_ptr<Cache::Function::FunctionRealization>&)
+      const std::shared_ptr<AST::Cache::Function::FunctionRealization>&)
       -> Pud::IR::Func*;
 
  private:
@@ -227,43 +232,43 @@ class TypecheckVisitor : public CallbackASTVisitor<ExprPtr, StmtPtr> {
       -> Pud::Type::FuncTypePtr;
   auto find_best_method(const Pud::Type::ClassTypePtr& typ,
                         const std::string& member,
-                        const std::vector<ExprPtr>& args)
+                        const std::vector<AST::ExprPtr>& args)
       -> Pud::Type::FuncTypePtr;
   auto find_best_method(
       const Pud::Type::ClassTypePtr& typ, const std::string& member,
       const std::vector<std::pair<std::string, Pud::Type::TypePtr>>& args)
       -> Pud::Type::FuncTypePtr;
   auto can_call(const Pud::Type::FuncTypePtr&,
-                const std::vector<CallExpr::Arg>&) -> int;
+                const std::vector<AST::CallExpr::Arg>&) -> int;
   auto find_matching_methods(const Pud::Type::ClassTypePtr& typ,
                              const std::vector<Pud::Type::FuncTypePtr>& methods,
-                             const std::vector<CallExpr::Arg>& args)
+                             const std::vector<AST::CallExpr::Arg>& args)
       -> std::vector<Pud::Type::FuncTypePtr>;
-  auto wrap_expr(ExprPtr& expr, const Pud::Type::TypePtr& expected_type,
+  auto wrap_expr(AST::ExprPtr& expr, const Pud::Type::TypePtr& expected_type,
                  const Pud::Type::FuncTypePtr& callee = nullptr,
                  bool allow_unwrap = true) -> bool;
-  auto cast_to_super_class(ExprPtr expr, Pud::Type::ClassTypePtr super_typ,
-                           bool = false) -> ExprPtr;
-  auto prepare_vtables() -> StmtPtr;
+  auto cast_to_super_class(AST::ExprPtr expr, Pud::Type::ClassTypePtr super_typ,
+                           bool = false) -> AST::ExprPtr;
+  auto prepare_vtables() -> AST::StmtPtr;
 
  public:
   auto is_tuple(const std::string& s) const -> bool { return s == TYPE_TUPLE; }
   auto get_class_fields(Pud::Type::ClassType*)
-      -> std::vector<Cache::Class::ClassField>&;
+      -> std::vector<AST::Cache::Class::ClassField>&;
 
   friend class Cache;
   friend class Pud::Type::CallableTrait;
   friend class Pud::Type::UnionType;
 
  private:
-  auto unpack_tuple_types(ExprPtr) -> std::shared_ptr<
+  auto unpack_tuple_types(AST::ExprPtr) -> std::shared_ptr<
       std::vector<std::pair<std::string, Pud::Type::TypePtr>>>;
   auto transform_static_loop_call(
-      const std::vector<std::string>&, ExprPtr,
-      std::function<std::shared_ptr<SourceObject>(StmtPtr)>)
+      const std::vector<std::string>&, AST::ExprPtr,
+      std::function<std::shared_ptr<SourceObject>(AST::StmtPtr)>)
       -> std::pair<bool, std::vector<std::shared_ptr<SourceObject>>>;
 };
 
-}  // namespace Pud::AST
+}  // namespace Pud::Type
 
 #endif  // PUD_TYPECHECK_TYPECHECK_H
--- lib/Type/Static.cc
+++ lib/Type/Static.cc
@@ -7,18 +7,23 @@
 #include "Pud/AST/AST.h"
 #include "Pud/AST/Cache.h"
 #include "Pud/Common/Common.h"
+#include "Pud/Format/Format.h"
+#include "Pud/TypeCheck/TypeCheck.h"
 
 namespace Pud::Type {
 
 StaticType::StaticType(AST::Cache* cache, const std::shared_ptr<AST::Expr>& e)
     : Type(cache), expr(e->clone()) {
+  // 如果表达式未被静态求值或未被求值，它将调用 parse_expr 方法来分析
+  // 和填充 generics。
   if (!expr->is_static() || !expr->static_value.evaluated) {
     std::unordered_set<std::string> seen;
     parse_expr(expr, seen);
   }
 }
 
-StaticType::StaticType(AST::Cache* cache, std::vector<ClassType::Generic> generics,
+StaticType::StaticType(AST::Cache* cache,
+                       std::vector<ClassType::Generic> generics,
                        const std::shared_ptr<AST::Expr>& e)
     : Type(cache), generics(std::move(generics)), expr(e->clone()) {}
 
@@ -28,68 +33,86 @@ StaticType::StaticType(AST::Cache* cache, int64_t i)
 StaticType::StaticType(AST::Cache* cache, const std::string& s)
     : Type(cache), expr(std::make_shared<AST::StringExpr>(s)) {}
 
-int StaticType::unify(Type* typ, Unification* us) {
+// 用于统一两个静态类型。
+auto StaticType::unify(Type* typ, Unification* undo) -> int {
   if (auto t = typ->get_static()) {
-    if (can_realize())
+    // 检查当前实例 (this) 或传入的类型 t 是否可以实现（can_realize()）
+    // 则对其表达式进行求值。
+    if (can_realize()) {
       expr->static_value = evaluate();
-    if (t->can_realize())
+    }
+    if (t->can_realize()) {
       t->expr->static_value = t->evaluate();
-    // Check if both types are already evaluated.
-    if (expr->static_value.type != t->expr->static_value.type)
+    }
+    // 检查两个类型的求值结果类型是否相同。
+    if (expr->static_value.type != t->expr->static_value.type) {
       return -1;
-    if (expr->static_value.evaluated && t->expr->static_value.evaluated)
+    }
+    // 如果两个 StaticType 实例都已求值，则直接比较它们的值。
+    if (expr->static_value.evaluated && t->expr->static_value.evaluated) {
       return expr->static_value == t->expr->static_value ? 2 : -1;
-    else if (expr->static_value.evaluated && !t->expr->static_value.evaluated)
-      return typ->unify(this, us);
-
-    // Right now, *this is not evaluated
-    // Let us see can we unify it with other _if_ it is a simple IdExpr?
-    if (expr->get_id() && t->expr->static_value.evaluated) {
-      return generics[0].type->unify(typ, us);
+    } else if (expr->static_value.evaluated &&
+               !t->expr->static_value.evaluated) {
+      // 如果当前实例已求值但传入的类型未求值，尝试反向统一。
+      return typ->unify(this, undo);
     }
 
-    // At this point, *this is a complex expression (e.g. A+1).
-    //seqassert(!generics.empty(), "unevaluated simple expression");
-    if (generics.size() != t->generics.size())
-      return -1;
+    // 如果当前实例是一个简单的标识符表达式，并且传入类型已求值，
+    // 则尝试对泛型进行统一。
+    if (expr->get_id() && t->expr->static_value.evaluated) {
+      return generics[0].type->unify(typ, undo);
+    }
 
-    int s1 = 2, s = 0;
-    if (!(expr->get_id() && t->expr->get_id()) &&
-        expr->to_string() != t->expr->to_string())
+    // 如果当前实例是一个复杂表达式（如 A+1），则比较两个类型的泛型大小。
+    // seqassert(!generics.empty(), "unevaluated simple expression");
+    if (generics.size() != t->generics.size()) {
       return -1;
+    }
+
+    int s1 = 2;
+    int s = 0;
+    // 如果表达式形式不相同
+    if (!(expr->get_id() && t->expr->get_id()) &&
+        expr->to_string() != t->expr->to_string()) {
+      return -1;
+    }
+    // 遍历泛型，尝试逐个统一。
     for (int i = 0; i < generics.size(); i++) {
-      if ((s = generics[i].type->unify(t->generics[i].type.get(), us)) == -1)
+      s = generics[i].type->unify(t->generics[i].type.get(), undo);
+      if (s == -1) {
         return -1;
+      }
       s1 += s;
     }
     return s1;
   } else if (auto tl = typ->get_link()) {
-    return tl->unify(this, us);
+    return tl->unify(this, undo);
   }
   return -1;
 }
 
-TypePtr StaticType::generalize(int atLevel) {
+auto StaticType::generalize(int at_level) -> TypePtr {
   auto e = generics;
   for (auto& t : e)
-    t.type = t.type ? t.type->generalize(atLevel) : nullptr;
+    t.type = t.type ? t.type->generalize(at_level) : nullptr;
   auto c = std::make_shared<StaticType>(cache, e, expr);
   c->set_source_info(get_source_info());
   return c;
 }
 
-TypePtr StaticType::instantiate(int atLevel, int* unboundCount,
-                                std::unordered_map<int, TypePtr>* cache) {
+auto StaticType::instantiate(int at_level, int* unbound_count,
+                             std::unordered_map<int, TypePtr>* cache)
+    -> TypePtr {
   auto e = generics;
   for (auto& t : e)
     t.type =
-        t.type ? t.type->instantiate(atLevel, unboundCount, cache) : nullptr;
+        t.type ? t.type->instantiate(at_level, unbound_count, cache) : nullptr;
   auto c = std::make_shared<StaticType>(this->cache, e, expr);
   c->set_source_info(get_source_info());
   return c;
 }
 
-std::vector<TypePtr> StaticType::get_unbounds() const {
+auto StaticType::get_unbounds() const -> std::vector<TypePtr> {
   std::vector<TypePtr> u;
   for (auto& t : generics)
     if (t.type) {
@@ -99,7 +122,7 @@ std::vector<TypePtr> StaticType::get_unbounds() const {
   return u;
 }
 
-bool StaticType::can_realize() const {
+auto StaticType::can_realize() const -> bool {
   if (!expr->static_value.evaluated)
     for (auto& t : generics)
       if (t.type && !t.type->can_realize())
@@ -107,9 +130,11 @@ bool StaticType::can_realize() const {
   return true;
 }
 
-bool StaticType::isInstantiated() const { return expr->static_value.evaluated; }
+auto StaticType::is_instantiated() const -> bool {
+  return expr->static_value.evaluated;
+}
 
-std::string StaticType::debug_string(char mode) const {
+auto StaticType::debug_string(char mode) const -> std::string {
   if (expr->static_value.evaluated)
     return expr->static_value.to_string();
   if (mode == 2) {
@@ -118,62 +143,66 @@ std::string StaticType::debug_string(char mode) const {
       s.push_back(g.type->debug_string(mode));
     return fmt::format("Static[{};{}]", join(s, ","), expr->to_string());
   } else {
-    return fmt::format("Static[{}]", FormatVisitor::apply(expr));
+    return fmt::format("Static[{}]", AST::FormatVisitor::apply(expr));
   }
 }
 
-std::string StaticType::realized_name() const {
-  seqassert(can_realize(), "cannot realize {}", to_string());
+auto StaticType::realized_name() const -> std::string {
+  // seqassert(can_realize(), "cannot realize {}", to_string());
   std::vector<std::string> deps;
   for (auto& e : generics)
     deps.push_back(e.type->realized_name());
   if (!expr->static_value.evaluated)  // If not already evaluated, evaluate!
     const_cast<StaticType*>(this)->expr->static_value = evaluate();
-  seqassert(expr->static_value.evaluated, "static value not evaluated");
+  // seqassert(expr->static_value.evaluated, "static value not evaluated");
   return expr->static_value.to_string();
 }
 
-StaticValue StaticType::evaluate() const {
-  if (expr->static_value.evaluated)
+auto StaticType::evaluate() const -> AST::StaticValue {
+  if (expr->static_value.evaluated) {
     return expr->static_value;
-  cache->typeCtx->addBlock();
-  for (auto& g : generics)
-    cache->typeCtx->add(TypecheckItem::Type, g.name, g.type);
-  auto oldChangedNodes = cache->typeCtx->changedNodes;
-  auto en = TypecheckVisitor(cache->typeCtx).transform(expr->clone());
-  cache->typeCtx->changedNodes = oldChangedNodes;
-  seqassert(en->isStatic() && en->static_value.evaluated,
-            "{} cannot be evaluated", en);
-  cache->typeCtx->popBlock();
+  }
+  cache->type_ctx->add_block();
+  for (auto& g : generics) {
+    cache->type_ctx->add(TypecheckItem::Type, g.name, g.type);
+  }
+  auto old_changed_nodes = cache->type_ctx->changed_nodes;
+  auto en = TypecheckVisitor(cache->type_ctx).transform(expr->clone());
+  cache->type_ctx->changed_nodes = old_changed_nodes;
+  // seqassert(en->is_static() && en->static_value.evaluated,
+  //           "{} cannot be evaluated", en);
+  cache->type_ctx->pop_block();
   return en->static_value;
 }
 
-void StaticType::parseExpr(const ExprPtr& e,
-                           std::unordered_set<std::string>& seen) {
+void StaticType::parse_expr(const AST::ExprPtr& e,
+                            std::unordered_set<std::string>& seen) {
   e->type = nullptr;
   if (auto ei = e->get_id()) {
     if (!in(seen, ei->value)) {
-      auto val = cache->typeCtx->find(ei->value);
-      seqassert(val && val->type->isStaticType(), "invalid static expression");
-      auto genTyp = val->type->follow();
-      auto id = genTyp->getLink() ? genTyp->getLink()->id
-                : genTyp->getStatic()->generics.empty()
+      auto val = cache->type_ctx->find(ei->value);
+      // seqassert(val && val->type->is_static_type(), "invalid static
+      // expression");
+      auto gen_typ = val->type->follow();
+      auto id = gen_typ->get_link() ? gen_typ->get_link()->id
+                : gen_typ->get_static()->generics.empty()
                     ? 0
-                    : genTyp->getStatic()->generics[0].id;
+                    : gen_typ->get_static()->generics[0].id;
       generics.emplace_back(ClassType::Generic(
-          ei->value, cache->typeCtx->cache->reverseIdentifierLookup[ei->value],
-          genTyp, id));
+          ei->value,
+          cache->type_ctx->cache->reverse_identifier_lookup[ei->value], gen_typ,
+          id));
       seen.insert(ei->value);
     }
-  } else if (auto eu = e->getUnary()) {
-    parseExpr(eu->expr, seen);
-  } else if (auto eb = e->getBinary()) {
-    parseExpr(eb->lexpr, seen);
-    parseExpr(eb->rexpr, seen);
-  } else if (auto ef = e->getIf()) {
-    parseExpr(ef->cond, seen);
-    parseExpr(ef->ifexpr, seen);
-    parseExpr(ef->elsexpr, seen);
+  } else if (auto eu = e->get_unary()) {
+    parse_expr(eu->expr, seen);
+  } else if (auto eb = e->get_binary()) {
+    parse_expr(eb->lexpr, seen);
+    parse_expr(eb->rexpr, seen);
+  } else if (auto ef = e->get_if()) {
+    parse_expr(ef->cond, seen);
+    parse_expr(ef->ifexpr, seen);
+    parse_expr(ef->elsexpr, seen);
   }
 }
 
